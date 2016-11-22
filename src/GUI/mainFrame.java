313bb43f22abc90564ffe97c556e5304aa3d0a11
Ahmed Lotfey <A7med.lotfey@gmail.com>
2016-11-22 02:14:25 +0200

    Initial Commit

--- src/GUI/mainFrame.java
+++ src/GUI/mainFrame.java
@@ -22,11 +22,12 @@ public class mainFrame {
 
 
     private mainFrame() {
+        createUIComponents();
     }
 
     private static mainFrame frame = null;
 
-    private static JPanel getMainPanel() {
+    public static JPanel getMainPanel() {
         if (frame == null)
             return (frame = new mainFrame()).mainPanel;
         return frame.mainPanel;
@@ -39,21 +40,15 @@ public class mainFrame {
         DefaultTableModel defaultTableModel = new DefaultTableModel();
         defaultTableModel.addColumn("Index");
         defaultTableModel.addColumn("Data");
-        String data[][] = new String[memory.length][2];
-        for (int i = 0; i < 265; i++) {
-            data[i] = new String[]{Integer.toString(i + 1), Integer.toString(memory[i])};
-            defaultTableModel.addRow(data[i]);
+        String data[][] = new String[256][2];
+        for (int i = 0; i <50; i++) {
+            memory[i] = 0;
+            defaultTableModel.addRow(new String[]{Integer.toString(i + 1), Integer.toString(memory[i])});
         }
         memoryTable.setModel(defaultTableModel);
         // end of testing
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("mainFrame");
-        frame.setContentPane(getMainPanel());
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
-    }
+
 
 }
